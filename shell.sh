7fb624a3a3383c655f081687dd0ae1365a8f11da
amit <amitkumar1000091@gmail.com>
2020-06-30 07:00:06 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,7 +1,13 @@
-
-ls
-#present working directory
-pwd
-echo "what is your name "
-read name
-echo "how do you do $name"
+#update all package 
+echo "updating all package first"
+sudo apt update
+echo "all package updated successfully"
+printf "\n\n\n\n"
+echo "Please wait ..."
+sleep 2 
+echo "Next, install a few prerequisite packages which let apt use packages over HTTPS:"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+echo "success"
+printf "\n\n\n\n"
+echo "Please wait ..."
+sleep 2 
